0ccb864aa5d31cd1c40dfb38ec5613b2c5ca23b4
Kákonyi Regina <h879132@stud.u-szeged.hu>
2023-11-10 15:05:38 +0100

    3. Single input - Test

--- tests/greetings.test.ts
+++ tests/greetings.test.ts
@@ -11,12 +11,21 @@ describe('No input', () => {
         expect(greet()).toBe("Hello, my friend.");
     })
     test('Empty array as input', () => {
-        expect(greet([])).toBe("Hello, my friend.");
+        expect(greet()).toBe("Hello, my friend.");
     })
 })
 
 describe('Single input', () => {
     test('Normal input', () => {
         expect(greet("Alice")).toBe("Hello Alice.");
+    })
+    test('Unnecessary space in the beginning', () => {
+        expect(greet(" Alice")).toBe("Hello Alice.");
+    })
+    test('Unnecessary space at the end', () => {
+        expect(greet("Alice ")).toBe("Hello Alice.");
+    })
+    test('Unnecessary spaces at both ends', () => {
+        expect(greet(" Alice ")).toBe("Hello Alice.");
     })
 })
