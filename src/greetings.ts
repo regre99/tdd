b4f5bcecf8f48bc509a45b467ce9afb515e04477
Kákonyi Regina <h879132@stud.u-szeged.hu>
2023-11-10 15:06:32 +0100

    3. Single input - Implementation

--- src/greetings.ts
+++ src/greetings.ts
@@ -1,4 +1,8 @@
 export const greet = (names: string = "") => {
     if (names.length ===  0) return "Hello, my friend.";
+    while (names[0] === " " || names[names.length-1] === " ") {
+        if (names[0] === " ") names = names.slice(1, names.length);
+        if (names[names.length-1] === " ") names = names.slice(0, names.length-1);
+    }
     return "Hello " + names + ".";
 }
