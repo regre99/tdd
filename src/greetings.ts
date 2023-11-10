8be8ab571093066b0f536441d544f8479a8b02c6
Kákonyi Regina <h879132@stud.u-szeged.hu>
2023-11-10 18:54:46 +0100

    4. Multiple input - Implementation

--- src/greetings.ts
+++ src/greetings.ts
@@ -1,8 +1,21 @@
 export const greet = (names: string = "") => {
-    if (names.length ===  0) return "Hello, my friend.";
-    while (names[0] === " " || names[names.length-1] === " ") {
-        if (names[0] === " ") names = names.slice(1, names.length);
-        if (names[names.length-1] === " ") names = names.slice(0, names.length-1);
+    if (names.length === 0) return "Hello, my friend.";
+    var namesList = names.split(", ");
+    console.log(namesList);
+    for (var i = 0; i < namesList.length; i++) {
+        while (namesList[i][0] === " " || namesList[i][namesList[i].length - 1] === " ") {
+            if (namesList[i][0] === " ") namesList[i] = namesList[i].slice(1, names.length);
+            if (namesList[i][namesList[i].length - 1] === " ") namesList[i] = namesList[i].slice(0, namesList[i].length - 1);
+        }
     }
-    return "Hello " + names + ".";
+    if (namesList.length > 1) {
+        var greeting = "Hello ";
+        for (var i = 0; i < namesList.length-2; i++) {
+            greeting += namesList[i] + ", ";
+        }
+        greeting += namesList[namesList.length-2] + " and " + namesList[namesList.length-1] + ".";
+        return greeting;
+    }
+    return "Hello " + namesList[0] + ".";
 }
+
