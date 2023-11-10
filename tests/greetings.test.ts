3e3d004e89a1a1c0976ccd7e60e732f2f8cbad33
Kákonyi Regina <h879132@stud.u-szeged.hu>
2023-11-10 14:35:33 +0100

    2. No input - Test

--- tests/greetings.test.ts
+++ tests/greetings.test.ts
@@ -1,5 +1,16 @@
 import { greet } from '../src/greetings'
 
-test('Function "greet" exists', () => {
-    expect(greet()).toBeDefined();
+describe('Function "greet" exists', () => {
+    test('Function "greet" exists', () => {
+        expect(greet()).toBeDefined();
+    })
 });
+
+describe('No input', () => {
+    test('No input', () => {
+        expect(greet()).toBe("Hello, my friend.");
+    })
+    test('Empty array as input', () => {
+        expect(greet([])).toBe("Hello, my friend.");
+    })
+})
